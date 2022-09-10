f19dd36cc4a128a38864cdbf137ae344722afec6
lan-mak <d.makoseev@elec-co.ru>
2022-09-10 19:38:25 +0300

    temp

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,23 @@
-// TODO: write code here
+class Game {
+  constructor(width = 4, height = 4) {
+    this.width = width;
+    this.height = height;
+    this.countCell = this.width * this.height;
+    this.sizeCell = 250;
+  }
 
-// comment this to pass build
-// const unusedVariable = 'variable';
+  newGame() {
+    const body = document.querySelector('body');
+    body.insertAdjacentHTML('afterbegin', '<div class="board"></div>');
 
-// for demonstration purpose only
-export default function demo(value) {
-  return value;
+    console.log(`привет ${this.width} и ${this.height}`);
+
+    const cell = document.createElement('div');
+    cell.style.width = '250px';
+    cell.style.height = '250px';
+    cell.innerText = 'test';
+    console.log(cell);
+  }
 }
 
-console.log('app.js included');
+window.widget = new Game();
