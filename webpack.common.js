daf1960518272b41010b0d10bdd9ad5ac8f6fffd
lan-mak <d.makoseev@elec-co.ru>
2022-09-13 21:10:41 +0300

    upd

--- webpack.common.js
+++ webpack.common.js
@@ -12,10 +12,13 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
         },
       },
       {
